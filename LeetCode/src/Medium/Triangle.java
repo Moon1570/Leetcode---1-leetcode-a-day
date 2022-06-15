c6310dcf6a8c98b61560421a721a060ce00c0aab
Moon1570 <moon.cse4.bu@gmail.com>
2022-06-15 09:22:22 +0600

    Second day
    120. Triangle

---
+++ LeetCode/src/Medium/Triangle.java
@@ -0,0 +1,23 @@
+package Medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int sum = 0;
+        for(int i=triangle.size()-2; i>=0; i--){
+            for(int j=0; j<triangle.get(i).size();j++){
+                int min = Math.min(triangle.get(i+1).get(j), triangle.get(i+1).get(j+1));
+                sum = min + triangle.get(i).get(j);
+                triangle.get(i).set(j, sum);
+            }
+       }
+        
+        return triangle.get(0).get(0);
+    }
+ 
+}
