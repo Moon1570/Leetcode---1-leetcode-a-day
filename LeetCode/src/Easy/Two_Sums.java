c90dfc9e1bbc9118624fa3023063170091e0a915
Moon1570 <moon.cse4.bu@gmail.com>
2022-06-15 08:56:17 +0600

    First Day - First Commit
    1. Two Sum

---
+++ LeetCode/src/Easy/Two_Sums.java
@@ -0,0 +1,26 @@
+package Easy;
+
+import java.util.HashMap;
+import java.util.Iterator;
+
+public class Two_Sums {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] nums = {1, 2, 3, 4};
+		int target = 5;
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+		
+		for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                 System.out.println(map.get(complement)+ "  "+ i );
+            }
+            map.put(nums[i], i);
+        }
+        // In case there is no solution, we'll just return null
+		
+		System.out.println(map.get(2));
+	}
+
+}
