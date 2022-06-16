c9910c5b4a68543809ae1a9297753c6a6b1e4af0
Moon1570 <moon.cse4.bu@gmail.com>
2022-06-16 16:57:55 +0600

    Third Day

---
+++ LeetCode/src/Easy/Binary_Search_704.java
@@ -0,0 +1,29 @@
+package Easy;
+
+public class Binary_Search_704 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int[] nums = {5, 6};
+		int target = 5;
+		int left = 0;
+        int right = nums.length -1;
+        
+        while(left<=right){
+            int middle = (left + right)/2;
+            
+            if(target < nums[middle]){
+                right = middle-1;
+            } 
+            else if(target > nums[middle]){
+                left = middle+1;
+            }
+            else if (target == nums[middle]){
+                System.out.println(middle);
+            }
+        }
+        System.out.println(-1);
+	}
+
+}
